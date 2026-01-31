ea2a27477951d76fafb69017aa38db0eb3a3c4dc
Eli35 <eli357a@gmail.com>
2026-01-31 20:51:44 +0200

    added swagger & request rest

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import postsRouter from "./routes/Post";
 import commentsRouter from "./routes/Comment";
 import usersRouter from "./routes/User";
 import authRouter from "./routes/Auth";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./swagger";
 
 const app = express();
 
@@ -12,8 +14,10 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use("/auth", authRouter);
-app.use("/users", usersRouter)
+app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
 app.use("/comments", commentsRouter);
 
---
+++ src/swagger.ts
@@ -0,0 +1,20 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+export const swaggerSpec = swaggerJSDoc({
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Rest API",
+      version: "2.0.0",
+      description: "REST API",
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+  }});
